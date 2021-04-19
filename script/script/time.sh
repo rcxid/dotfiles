54fe46d456005da5535304147a253b4603c7b66e
rcxid <gc6174@gmail.com>
2021-04-19 09:59:02 +0800

    修改时间显示格式

--- script/script/time.sh
+++ script/script/time.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ true ]; do
-	#date +"%Y-%m-%d %H:%M:%S"
-	xsetroot -name "$(date +"%Y-%m-%d %H:%M")"
+	#date +"%Y-%m-%d %H:%M:%S %a"
+	xsetroot -name "$(date +"%m-%d %H:%M %a")"
 	sleep 1
 done
