33eaaee3b63d48251be4a2e465260a0cc050c4c4
rcxid <gc6174@gmail.com>
2021-04-27 10:59:46 +0800

    增加程序启动脚本

---
+++ script/script/startup.sh
@@ -0,0 +1,14 @@
+# 运行输入法
+~/script/fcitx5.sh
+# 设置屏幕分辨率
+~/script/screen.sh
+# 壁纸设置
+~/script/picture.sh
+# 窗口通明
+~/script/xcompmgr.sh
+# dwm bar
+~/script/bar.sh
+# 自动锁定小键盘区
+~/script/numlockx
+# 启动桌面
+exec dwm
