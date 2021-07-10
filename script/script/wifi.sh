a260059ec07b1db2dfc115a056401333099e4b91
rcxid <gc6174@gmail.com>
2021-07-10 23:18:19 +0800

    添加手机热点wifi配置

--- script/script/wifi.sh
+++ script/script/wifi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup wpa_supplicant -c /home/vision/script/conf/wlan_fm.conf -i wlp4s0 > /dev/null 2>&1 &
+nohup wpa_supplicant -c /home/vision/script/conf/wlan_realme.conf -i wlp4s0 > /dev/null 2>&1 &
