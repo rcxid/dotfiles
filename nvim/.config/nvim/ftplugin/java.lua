ceed52000e6b0e635b45beefc1a660e525649430
rcxid <gc6174@gmail.com>
2022-02-14 07:58:07 +0000

    feat: nvim-jdtls配置完善

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -121,3 +121,12 @@ local mappings = {
 
 -- 加载按键映射配置
 require('core.keymap').load_mapping_configs(mappings)
+
+vim.cmd([[
+command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)
+command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_set_runtime JdtSetRuntime lua require('jdtls').set_runtime(<f-args>)
+command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()
+command! -buffer JdtJol lua require('jdtls').jol()
+command! -buffer JdtBytecode lua require('jdtls').javap()
+command! -buffer JdtJshell lua require('jdtls').jshell()
+]])
