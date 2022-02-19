bb4936442069873a794c943a24bfc78c790a8111
rcxid <gc6174@gmail.com>
2022-02-19 22:42:49 +0800

    feat: nvim gopls

---
+++ nvim/.config/nvim/lua/plugins/golang.lua
@@ -0,0 +1,4 @@
+#!/bin/lua
+
+require('lspconfig').gopls.setup({
+})
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -5,18 +5,19 @@ local module = require('core.util.module')
 local prefix = 'plugins'
 
 local modules = {
-  'packer', 
-  'lspconfig', 
-  'nvim-cmp', 
-  'snippets', 
-  'rnvimr', 
-  'vim-choosewin', 
-  'vim-airline', 
-  'vim-floaterm', 
-  'nvim-tree', 
-  'rust', 
-  'python', 
-  'tagbar', 
+  'packer',
+  'lspconfig',
+  'nvim-cmp',
+  'snippets',
+  'rnvimr',
+  'vim-choosewin',
+  'vim-airline',
+  'vim-floaterm',
+  'nvim-tree',
+  'rust',
+  'python',
+  'golang',
+  'tagbar',
 }
 
 module.load_modules(modules, prefix)
