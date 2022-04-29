046d0bdbe1f46105212db657adddb87caa8471ce
rcxid <gc6174@gmail.com>
2022-04-30 03:10:39 +0800

    feat: 更新git相关配置

--- install.sh
+++ install.sh
@@ -2,5 +2,14 @@
 
 dir=$(pwd)
 
-rm -f "$HOME/.config/nvim"
-ln -s "$dir/nvim" "$HOME/.config/nvim"
+echo "install nvim config"
+nvim_config_dir="$HOME/.config/nvim"
+rm -f "$nvim_config_dir"
+ln -s "$dir/nvim" "$nvim_config_dir"
+echo "ok"
+
+echo "install git config"
+git_config_dir="$HOME/.gitconfig"
+rm -f "$git_config_dir"
+ln -s "$dir/git/.gitconfig" "$git_config_dir"
+echo "ok"
