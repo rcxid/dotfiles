a298a9f850012966895794191e35a7aa1df2a134
rcxid <gc6174@gmail.com>
2022-06-06 09:15:23 +0000

    feat: add cmake mapping config

--- nvim/lua/config/mapping.lua
+++ nvim/lua/config/mapping.lua
@@ -58,10 +58,11 @@ local mappings = {
   { 'n',     '<space>df',    "<Cmd>lua require('jdtls').test_class()<CR>" },
   { 'n',     '<space>dn',    "<Cmd>lua require('jdtls').test_nearest_method()<CR>" },
   -- cmake
-  { 'n',     '<space>cc', ':CMake create_project<CR>', options },
-  { 'n',     '<space>cb', ':CMake build_all<CR>', options },
-  { 'n',     '<space>cr', ':CMake run<CR>', options },
-  { 'n',     '<space>cs', ':CMake set_target_args<CR>', options },
+  { 'n',     '<space>cc', ':CMake create_project<CR>',  options },
+  { 'n',     '<space>cb', ':CMake build_all<CR>',       options },
+  { 'n',     '<space>cr', ':CMake run<CR>',             options },
+  { 'n',     '<space>ca', ':CMake set_target_args<CR>', options },
+  { 'n',     '<space>cs', ':CMake select_target<CR>',   options },
 }
 
 -- 加载按键映射配置
