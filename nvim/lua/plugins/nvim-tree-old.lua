74d91a2f0bfe71270b515df3ade91ed53bf3c4c5
rcxid <gc6174@gmail.com>
2022-08-27 14:40:40 +0800

    fix: update nvim-tree configuration.

---
+++ nvim/lua/plugins/nvim-tree-old.lua
@@ -0,0 +1,105 @@
+#!/bin/lua
+
+local status_ok, nvim_tree = pcall(require, 'nvim-tree')
+if not status_ok then
+  vim.notify('nvim-tree not found!')
+  return
+end
+
+vim.g.nvim_tree_icons = {
+  default = "",
+  symlink = "",
+  git = {
+    -- unstaged = "",
+    unstaged = "✗",
+    -- staged = "s",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    deleted = "",
+    -- untracked = "u",
+    untracked = "★",
+    ignored = "◌",
+  },
+  folder = {
+    arrow_open = "",
+    arrow_closed = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+    symlink_open = "",
+  },
+}
+
+-- following options are the default
+-- each of these are documented in `:help nvim-tree.OPTION_NAME`
+nvim_tree.setup {
+  disable_netrw        = true,
+  hijack_netrw         = true,
+  open_on_setup        = false,
+  ignore_ft_on_setup   = {},
+  -- auto_close           = false,
+  auto_reload_on_write = true,
+  open_on_tab          = false,
+  hijack_cursor        = false,
+  update_cwd           = false,
+  hijack_directories   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  git = {
+    enable = true,
+    ignore = true,
+    timeout = 500,
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    -- auto_resize = true,
+    mappings = {
+      custom_only = false,
+      list = {}
+    },
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes"
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true
+  },
+  actions = {
+    change_dir = {
+      global = false,
+    },
+    open_file = {
+      quit_on_open = false,
+    }
+  }
+}
