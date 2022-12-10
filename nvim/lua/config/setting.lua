ebb2dfb28584a4c8cd700cbe2cc7a04cb8048bab
rcxid <gc6174@gmail.com>
2022-12-10 18:22:18 +0800

    调整缩进

--- nvim/lua/config/setting.lua
+++ nvim/lua/config/setting.lua
@@ -3,37 +3,37 @@
 local setting = require('core.setting')
 
 local options = {
-    number          = true;         -- 设置行号
-    relativenumber  = true;         -- 设置相对行号
-    autoindent      = true;         -- 开启自动缩进
-    smartindent     = true;
-    expandtab       = true;         -- 空格填充tab
-    hidden          = true;
-    tabstop         = 4;            -- tab长度
-    shiftwidth      = 4;            -- tab缩进
-    softtabstop     = 4;
-    pumheight       = 10;
-    encoding        = 'utf-8';      -- 字符编码
-    fileencoding    = 'utf-8';      -- 写到文件的字符编码
-    cursorline      = true;         -- 高亮所在行
-    colorcolumn     = '80';         -- 开启右侧参考线
-    scrolloff       = 8;            -- jk移动时光标下上方保留8行
-    sidescrolloff   = 8;
-    splitbelow      = true;
-    splitright      = true;
-    termguicolors   = true;
-    mouse           = 'a';          -- 启用鼠标
-    foldmethod      = 'expr';
-    foldexpr        = 'nvim_treesitter#foldexpr()';
-    foldenable      = false;
-    foldlevel       = 99;
+  number          = true;         -- 设置行号
+  relativenumber  = true;         -- 设置相对行号
+  autoindent      = true;         -- 开启自动缩进
+  smartindent     = true;
+  expandtab       = true;         -- 空格填充tab
+  hidden          = true;
+  tabstop         = 4;            -- tab长度
+  shiftwidth      = 4;            -- tab缩进
+  softtabstop     = 4;
+  pumheight       = 10;
+  encoding        = 'utf-8';      -- 字符编码
+  fileencoding    = 'utf-8';      -- 写到文件的字符编码
+  cursorline      = true;         -- 高亮所在行
+  colorcolumn     = '80';         -- 开启右侧参考线
+  scrolloff       = 8;            -- jk移动时光标下上方保留8行
+  sidescrolloff   = 8;
+  splitbelow      = true;
+  splitright      = true;
+  termguicolors   = true;
+  mouse           = 'a';          -- 启用鼠标
+  foldmethod      = 'expr';
+  foldexpr        = 'nvim_treesitter#foldexpr()';
+  foldenable      = false;
+  foldlevel       = 99;
 }
 
 local variables = {
-    {'mapleader', '\\'},
-    {'python3_host_prog', '/usr/bin/python3'},
-    -- when save buffer will auto exec :RustFmt
-    {'rustfmt_autosave', 1},
+  {'mapleader', '\\'},
+  {'python3_host_prog', '/usr/bin/python3'},
+  -- when save buffer will auto exec :RustFmt
+  {'rustfmt_autosave', 1},
 }
 
 vim.cmd([[
