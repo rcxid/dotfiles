5b8286e2bc310785839bc26f9089a02850638a4d
rcxid <gc6174@gmail.com>
2023-08-11 00:09:18 +0800

    feat: update nvim config.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,3 +1,27 @@
 return {
-  "neovim/nvim-lspconfig"
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local servers = {
+      rust_analyzer = {}
+    }
+    local on_attach = function()
+      -- todo
+    end
+    require("mason").setup()
+    require("mason-lspconfig").setup {
+      ensure_installed = vim.tbl_keys(servers),
+      handlers = {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            settings = servers[server_name],
+            on_attach = on_attach,
+          }
+        end
+      }
+    }
+  end
 }
--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,10 +1,13 @@
+-- 设置onedark主题
+local set_onedark_theme = function()
+  local onedark = require("onedark")
+  onedark.setup {
+    style = "warmer"
+  }
+  onedark.load()
+end
+
 return {
   "navarasu/onedark.nvim",
-  config = function()
-    local onedark = require("onedark")
-    onedark.setup {
-      style = "warmer"
-    }
-    onedark.load()
-  end
+  config = set_onedark_theme
 }
