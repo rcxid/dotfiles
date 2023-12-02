79782fed933a5b3c2f11c2d93b200b1d268bc95d
rcxid <gc6174@gmail.com>
2023-12-02 16:51:38 +0800

    feat: 更新wezterm配置

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,14 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
-return {
-  font = wezterm.font('DejaVuSansMono Nerd Font'),
-  font_size = 16.0,
-  color_scheme = "Gruvbox dark, hard (base16)",
+-- local font_config = { weight = "Regular", italic = false }
+local config = {
+  font_size = 19,
+  font = wezterm.font("CaskaydiaCove Nerd Font Mono"),
+  term = "xterm-256color",
+  -- color_scheme = "Gruvbox dark, hard (base16)",
+  initial_cols = 125,
+  initial_rows = 34,
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
 }
+
+return config
